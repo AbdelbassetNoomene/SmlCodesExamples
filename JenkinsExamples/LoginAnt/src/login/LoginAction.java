ea91affa637ddf0ea5f9b8c493266ff9bfbe39a3
SmlCodes <smlcodes@gmail.com>
2017-03-19 23:57:30 +0530

    jenkins

---
+++ JenkinsExamples/LoginAnt/src/login/LoginAction.java
@@ -0,0 +1,30 @@
+package login;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginAction extends HttpServlet {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		String n = request.getParameter("username");
+		String p = request.getParameter("userpass");
+
+		if (n.equalsIgnoreCase(p)) {
+			out.print("<h1>WelCome " + n + " !!");
+		} else {
+			out.print("<h1>Error!!! Get out from Here!!");
+
+		}
+
+		out.close();
+	}
+}
